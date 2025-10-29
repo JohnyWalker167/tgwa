f1ea4e11d3baa5ea04fb661e6a549cd1a25aa107
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2025-10-30 01:24:07 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,14 +1,38 @@
 
+import redis
+import json
+from config import REDIS_HOST, REDIS_PORT, REDIS_USERNAME, REDIS_PASSWORD
 from cachetools import TTLCache
 
-# Cache for user file counts
+# In-memory caches
 user_file_count = TTLCache(maxsize=1000, ttl=3600)
-
-# Cache for query IDs
 query_id_map = TTLCache(maxsize=1000, ttl=300)
-
-# Cache for search API results
 search_api_cache = TTLCache(maxsize=100, ttl=300)
-
-# Cache for search results
 search_cache = TTLCache(maxsize=100, ttl=300)
+
+
+if REDIS_HOST:
+    redis_client = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        decode_responses=True,
+        username=REDIS_USERNAME,
+        password=REDIS_PASSWORD,
+    )
+else:
+    redis_client = None
+
+def get(key):
+    if not redis_client:
+        return None
+    
+    value = redis_client.get(key)
+    if value:
+        return json.loads(value)
+    return None
+
+def set(key, value, ttl=300):
+    if not redis_client:
+        return
+        
+    redis_client.set(key, json.dumps(value), ex=ttl)
