9b8599081152d0239a01f642638e4fda56ac9c77
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2025-11-02 20:43:26 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -30,7 +30,7 @@ from mutagen.flac import FLAC
 from mutagen.mp4 import MP4
 from mutagen.id3 import ID3, APIC
 from mutagen import File as MutagenFile
-from cache import cache
+from cache import invalidate_cache
 
 
 async def upload_to_imgbb(image_url):
@@ -631,6 +631,7 @@ async def file_queue_worker(bot):
             logger.error(f"❌ Error saving file: {e}")
         finally:
             file_queue.task_done()
+            invalidate_cache()
 
 # =========================
 # Unified File Queueing
